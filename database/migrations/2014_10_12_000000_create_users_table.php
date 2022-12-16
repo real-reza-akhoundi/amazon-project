cc8a8aa60b581aad1821d29fbf2ae2e4aa1a71d5
Reza Akhoundi <rezaakhoundi1998@gmail.com>
2022-12-16 17:19:13 +0330

    feat(project) : install jetstream on project

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,23 +4,32 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('national_code')->unique()->nullable();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('slug')->unique()->nullable();
+            $table->text('profile_photo_path')->nullable()->comment('avatar');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->tinyInteger('activation')->default(0)->comment("0 => inactive , 1 => active");
+            $table->timestamp('activation_date')->nullable();
+            $table->tinyInteger('user_type')->default(0)->comment("0 => user , 1 => admin");
+            $table->tinyInteger('status')->default(0);
             $table->rememberToken();
+            $table->foreignId('current_team_id')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -29,8 +38,7 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('users');
     }
-}
+};
